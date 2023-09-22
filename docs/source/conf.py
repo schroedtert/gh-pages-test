9fe43e6620e55a019c17ae478fabeab0c3e7acab
Tobias Schrödter <t.schroedter@fz-juelich.de>
2023-09-22 13:26:55 +0200

    First docs

---
+++ docs/source/conf.py
@@ -0,0 +1,41 @@
+# Copyright © 2012-2023 Forschungszentrum Jülich GmbH
+# SPDX-License-Identifier: LGPL-3.0-or-later
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import datetime
+
+project = "Test"
+copyright = (
+    f"{datetime.datetime.today().year}"
+)
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+]
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "sphinx_book_theme"
+html_static_path = ["_static"]
+html_context = {"default_mode": "light"}
+
+html_theme_options = {
+    "home_page_in_toc": False,
+    "use_fullscreen_button": False,
+    "use_issues_button": False,
+    "use_download_button": False,
+}
+
+# -- Options for EPUB output
+epub_show_urls = "footnote"
